7b7f96c0c693047071bb709f979543d4cd2aaa6b
marco <marco@DESKTOP-GCUIOA1>
2026-03-03 18:36:40 +0200

    Add project files.

---
+++ G-NET-12-OOP03/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace G_NET_12_OOP03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region Part 1
+
+            #endregion
+
+
+
+
+
+
+        }
+    }
+}
