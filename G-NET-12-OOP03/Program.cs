f7e48684faab0525a7196462ddb36be97944f758
marco <marco@DESKTOP-GCUIOA1>
2026-03-03 19:25:05 +0200

    Q01

--- G-NET-12-OOP03/Program.cs
+++ G-NET-12-OOP03/Program.cs
@@ -36,43 +36,100 @@ namespace G_NET_12_OOP03
 
             #region Q02
 
-           // a) 
+            // a) 
 
-          // Child class in Diffrent Assembly  // Yes
+            // Child class in Diffrent Assembly  // Yes
 
-          // من خلال object instance برا الكلاس // No
+            // من خلال object instance برا الكلاس // No
 
-         // b) 
-         // protected internal
-         // Accessible :
-         // أي كلاس في نفس Assembly
-         // او أي Child class حتى لو Diffrent Assembly 
-          //private protected
-           // Accessible :
+            // b) 
+            // protected internal
+            // Accessible :
+            // أي كلاس في نفس Assembly
+            // او أي Child class حتى لو Diffrent Assembly 
+            //private protected
+            // Accessible :
 
-          // Child classes فقط
+            // Child classes فقط
 
-        // ولازم يكونوا في نفس Assembly
+            // ولازم يكونوا في نفس Assembly
 
-        // c) 
-         //  sealed class
-          // لا يمكن الوراثة منها
-          //sealed method
-          //لا يمكن override لها في child class
+            // c) 
+            //  sealed class
+            // لا يمكن الوراثة منها
+            //sealed method
+            //لا يمكن override لها في child class
 
-        // d) Create object from sealed class?
-        //  Yes
-       // لأن sealed يمنع inheritance فقط، مش إنشاء object.
+            // d) Create object from sealed class?
+            //  Yes
+            // لأن sealed يمنع inheritance فقط، مش إنشاء object.
+
+
+
+
+            #endregion
+
+            #endregion
+
+            #region Part 2
+
+            #region Q01
+
+
+            /*
+            بعمل كلاس جديد باسم تيكت وبكتب فيه ....
+
+                 public class Ticket
+             {
+            private static int counter = 0;
+
+            public int TicketId { get; }
+            public string MovieName { get; set; }
+
+            private decimal price;
+            public decimal Price
+            {
+                get => price;
+                set
+                {
+                    if (value <= 0)
+                        throw new ArgumentException("Price must be greater than 0");
+                    price = value;
+                }
+            }
+
+            public decimal PriceAfterTax => Price * 1.14m;
+
+            public Ticket(string movieName, decimal price)
+            {
+                TicketId = ++counter;
+                MovieName = movieName;
+                Price = price;
+            }
+
+            public static int GetTotalTickets()
+            {
+                return counter;
+            }
+
+            public override string ToString()
+            {
+                return $"Ticket #{TicketId} | {MovieName} | Price: {Price} EGP | After Tax: {PriceAfterTax:F2} EGP";
+            }
+            }
+
+
+            */
+            #endregion
+
+
+           
+
 
 
 
 
             #endregion
-
-
-            #endregion
-
-
 
 
 
---
+++ G-NET-12-OOP03/Ticket.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET_12_OOP03
+{
+    internal class Ticket
+    {
+        public class Ticket
+        {
+            private static int counter = 0;
+
+            public int TicketId { get; }
+            public string MovieName { get; set; }
+
+            private decimal price;
+            public decimal Price
+            {
+                get => price;
+                set
+                {
+                    if (value <= 0)
+                        throw new ArgumentException("Price must be greater than 0");
+                    price = value;
+                }
+            }
+
+            public decimal PriceAfterTax => Price * 1.14m;
+
+            public Ticket(string movieName, decimal price)
+            {
+                TicketId = ++counter;
+                MovieName = movieName;
+                Price = price;
+            }
+
+            public static int GetTotalTickets()
+            {
+                return counter;
+            }
+
+            public override string ToString()
+            {
+                return $"Ticket #{TicketId} | {MovieName} | Price: {Price} EGP | After Tax: {PriceAfterTax:F2} EGP";
+            }
+        }
+    }
+}
