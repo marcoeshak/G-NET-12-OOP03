9641e3b24007ec3286d4e6a4d90135a5e07566df
marco <marco@DESKTOP-GCUIOA1>
2026-03-03 19:05:32 +0200

    Q02

--- G-NET-12-OOP03/Program.cs
+++ G-NET-12-OOP03/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 using System.Collections;
+using System.Reflection;
 using System.Reflection.Metadata;
+using System.Security.AccessControl;
 
 namespace G_NET_12_OOP03
 {
@@ -32,6 +34,40 @@ namespace G_NET_12_OOP03
 
             #endregion
 
+            #region Q02
+
+           // a) 
+
+          // Child class in Diffrent Assembly  // Yes
+
+          // من خلال object instance برا الكلاس // No
+
+         // b) 
+         // protected internal
+         // Accessible :
+         // أي كلاس في نفس Assembly
+         // او أي Child class حتى لو Diffrent Assembly 
+          //private protected
+           // Accessible :
+
+          // Child classes فقط
+
+        // ولازم يكونوا في نفس Assembly
+
+        // c) 
+         //  sealed class
+          // لا يمكن الوراثة منها
+          //sealed method
+          //لا يمكن override لها في child class
+
+        // d) Create object from sealed class?
+        //  Yes
+       // لأن sealed يمنع inheritance فقط، مش إنشاء object.
+
+
+
+
+            #endregion
 
 
             #endregion
